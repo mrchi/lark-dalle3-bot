5e0780a3971cdc632def94f69b90ca9156fb8add
mrchi <i@mrchi.cc>
2023-11-09 01:40:27 +0800

    Refactor command help message generation

--- pkg/dispatcher/dispatcher.go
+++ pkg/dispatcher/dispatcher.go
@@ -1,7 +1,6 @@
 package dispatcher
 
 import (
-	"fmt"
 	"sort"
 	"strings"
 
@@ -9,22 +8,24 @@ import (
 )
 
 type Command struct {
-	Prefix      string
-	Description string
-	Execute     func(prompt string, larkeeClient *larkee.LarkClient, messageId string, tanantKey string)
+	Prefix  string
+	HelpMsg string
+	Execute func(prompt string, larkeeClient *larkee.LarkClient, messageId string, tanantKey string)
 }
 
 type CommandDispatcher struct {
 	larkeeClient       *larkee.LarkClient
 	prefixes           []string
+	helpMsgs           []string
 	prefixCommandMap   map[string]Command
-	commandHelpExecute func(helpMessage string, larkeeClient *larkee.LarkClient, messageId string, tanantKey string)
+	commandHelpExecute func(helpMsgs []string, larkeeClient *larkee.LarkClient, messageId string, tanantKey string)
 }
 
 func NewCommandDispatcher(larkeeClient *larkee.LarkClient, commands ...Command) *CommandDispatcher {
 	dispatcher := CommandDispatcher{larkeeClient: larkeeClient}
 	for _, command := range commands {
 		dispatcher.prefixes = append(dispatcher.prefixes, command.Prefix)
+		dispatcher.helpMsgs = append(dispatcher.helpMsgs, command.HelpMsg)
 		dispatcher.prefixCommandMap[command.Prefix] = command
 	}
 	// 按照前缀长度逆序排序，避免出现 /a /ab /abc 时，/ab 会被 /a 匹配的情况
@@ -32,15 +33,6 @@ func NewCommandDispatcher(larkeeClient *larkee.LarkClient, commands ...Command) 
 		return len(dispatcher.prefixes[i]) > len(dispatcher.prefixes[j])
 	})
 	return &dispatcher
-}
-
-func (dispatcher *CommandDispatcher) GenMarkdownHelpMsg() string {
-	var helpMessage []string
-	for _, prefix := range dispatcher.prefixes {
-		command := dispatcher.prefixCommandMap[prefix]
-		helpMessage = append(helpMessage, fmt.Sprintf("**%s** %s", command.Prefix, command.Description))
-	}
-	return strings.Join(helpMessage, "\n")
 }
 
 func (dispatcher *CommandDispatcher) Dispatch(text string, messageId string, tanantKey string) {
@@ -51,5 +43,5 @@ func (dispatcher *CommandDispatcher) Dispatch(text string, messageId string, tan
 			return
 		}
 	}
-	go dispatcher.commandHelpExecute(dispatcher.GenMarkdownHelpMsg(), dispatcher.larkeeClient, messageId, tanantKey)
+	go dispatcher.commandHelpExecute(dispatcher.helpMsgs, dispatcher.larkeeClient, messageId, tanantKey)
 }
